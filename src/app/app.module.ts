65cc5ce90d568b5800756ec70ed4efd4ea1d8168
Martin <martin_kili@hotmail.com>
2022-09-29 20:27:24 -0300

    V4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PadreComponent } from './components/padre/padre.component';
-import { HijoComponent } from './components/hijo/hijo.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PadreComponent,
-    HijoComponent
+    PadreComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/padre/padre.component.ts
+++ src/app/components/padre/padre.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./padre.component.css']
 })
 export class PadreComponent implements OnInit {
+  
+  valorNgClass!: number;
+  valorNgStyle!: number;
+  listaAlumnos: Array<string> = ['Martin', 'Mauricio', 'Maite'];
 
   constructor() { }
 
   ngOnInit(): void {
+
+    this.valorNgClass = 5;
+    this.valorNgStyle = 4;
   }
 
 }
